47bd16f4dbc174d04554b68a4298c12c2cec4d56
Jason Willems <jason@willems.ca>
2016-09-24 21:23:20 -0700

    Update to return N launches

--- src/index.js
+++ src/index.js
@@ -6,20 +6,18 @@ var request    = require('then-request');
 
 
 let APP_ID     = 'amzn1.ask.skill.e387cc51-e511-4e1a-8281-bc9b0eaadd30';
-let SKILL_NAME = 'Launch Manifest';
+let SKILL_NAME = 'Rocket Manifest';
 let BASE_URL   = 'https://launchlibrary.net/1.2/';
 let USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:48.0) Gecko/20100101 Firefox/48.0';
 
 
-
 var LaunchManifest = function () {
-    AlexaSkill.call(this, APP_ID);
+  AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
 LaunchManifest.prototype = Object.create(AlexaSkill.prototype);
 LaunchManifest.prototype.constructor = LaunchManifest;
-
 
 var request_headers = {
   'User-Agent': USER_AGENT,
@@ -27,33 +25,56 @@ var request_headers = {
 }
 
 
-
 LaunchManifest.prototype.intentHandlers = {
 
   'GetNextLaunch': function (intent, session, response) {
-    makeRequest('launch?next=1', function cb(err, data) {
-        console.log(data);
 
-        var speech_output;
-        var launch_info = data['launches'][0];
+    var launch_count = 1;
+    if (intent.slots.launchNum.value){
+      launch_count = (intent.slots.launchNum.value <= 10) ? intent.slots.launchNum.value : 10;
+    }
+    console.log("Launch count is " + launch_count); /* DEBUG */
 
-        var launch_name = launch_info.name ? launch_info.name.split('|')[1].replace('&', 'and') : null;
-        var confirmed = (launch_info.tbddate !== 1 && launch_info.tbdtime !== 1) ? true : false;
-        var launch_date = (launch_info.net !== '') ? launch_info.net : null;
+    makeRequest('launch?next=' + launch_count, function cb(err, data) {
+        console.log(data); /* DEBUG */
 
-        if (launch_name) {
-          speech_output = 'The next launch is ' + launch_name;
-          if (launch_date){
-            if (confirmed){
-              speech_output += ' which is confirmed for ' + launch_date;
+        var speech_output = '';
+        var returned_launch_count = 0;
+
+        for (var i=0; i<launch_count; i++) {
+          try {
+            var launch_info = data['launches'][i];
+
+            var launch_name = launch_info.name ? launch_info.name.split('|')[1].replace('&', 'and') : null;
+            var confirmed   = (launch_info.tbddate !== 1 && launch_info.tbdtime !== 1) ? true : false;
+            var launch_date = (launch_info.net !== '') ? launch_info.net.replace('UTC', 'U.T.C') : null;
+
+            if (launch_name) {
+              if (i == 0) {
+                speech_output += 'The next launch is ' + launch_name;
+              } else {
+                speech_output += 'The following launch is ' + launch_name
+              }
+              if (launch_date){
+                if (confirmed){
+                  speech_output += ' which is confirmed for ' + launch_date;
+                } else {
+                  speech_output += ' which is tentatively set for ' + launch_date;
+                }
+              }
+              speech_output += '. ';
+              returned_launch_count += 1;
             } else {
-              speech_output += ' which is tentatively set for ' + launch_date;
+              response.tellWithCard('There are no launches currently scheduled.');
             }
+          } catch(err) {
+            console.log('Error getting launch data');
           }
-        } else {
-          speech_output = 'There are no launches currently scheduled';
         }
 
+        if (returned_launch_count !== 1) {
+          speech_output = 'Here\'s your forcast for the next ' + returned_launch_count + ' launches. ' + speech_output;
+        }
         response.tellWithCard(speech_output);
     });
   },
